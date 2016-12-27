20bc1fba8288bb27a5c96d882ce6a39e1a06ac06
drokia2 <drokia2@gmail.com>
2016-12-27 12:54:11 -0800

    finished part 1

--- part_1.py
+++ part_1.py
@@ -59,58 +59,67 @@ from robot import *
 from math import *
 from matrix import *
 import random
-import time
-
-# This is the function you have to write. The argument 'measurement' is a 
-# single (x, y) point. This function will have to be called multiple
-# times before you have enough information to accurately predict the
-# next position. The OTHER variable that your function returns will be 
-# passed back to your function the next time it is called. You can use
-# this to keep track of important information over time.
 import math
-def estimate_next_pos(measurement, OTHER = None):
-    """Estimate the next (x, y) position of the wandering Traxbot
-    based on noisy (x, y) measurements."""
 
-    # You must return xy_estimate (x, y), and OTHER (even if it is None) 
-    # in this order for grading purposes.
-    if OTHER == None:
-        x = matrix([[test_target.heading], [test_target.distance], [0.], [0.]]) # initial state (location and velocity)
-        P =  matrix([[0,0,0,0],[0,0,0,0],[0,0,1000,0],[0,0,0,1000]])# initial uncertainty: 0 for positions x and y, 1000 for the two velocities
-        
-        prev_point = [test_target.x, test_target.y]
-    else:
-        x = OTHER['x']
-        P = OTHER['P']
-        
-        prev_point = OTHER['prev']    
+def convert_measurement_into_polar_coordinates(measurement, prev_point):
     dx = measurement[0] - prev_point[0]
     dy = measurement[1] - prev_point[1]
     m_heading = math.atan2(dy, dx)
     if m_heading < 0:
         m_heading += 2*pi
     m_distance = distance_between(prev_point, measurement)
+    return matrix([[m_heading, m_distance]])
 
-    # measurement update
-    Z = matrix([[m_heading, m_distance]])
-
+def kalman_prediction(x, P, Z):
     y = Z.transpose() - (H * x)
     S = H * P * H.transpose() + R
     K = P * H.transpose() * S.inverse()
     x = x + (K * y)
     P = (I - (K * H)) * P
-    
+
     # prediction
     x = (F * x) + u
     P = F * P * F.transpose()
-    
-    heading = x.value[0][0]
-    distance = x.value[1][0]
+    return x, P
+# This is the function you have to write. The argument 'measurement' is a 
+# single (x, y) point. This function will have to be called multiple
+# times before you have enough information to accurately predict the
+# next position. The OTHER variable that your function returns will be 
+# passed back to your function the next time it is called. You can use
+# this to keep track of important information over time.
+def estimate_next_pos(measurement, OTHER = None):
+    """Estimate the next (x, y) position of the wandering Traxbot
+    based on noisy (x, y) measurements."""
 
-    new_x = distance * cos(heading)
-    new_y = distance * sin(heading)
-    xy_estimate = [measurement[0] + new_x, measurement[1] + new_y]
-    return xy_estimate, {'x': x, 'P': P, 'prev': measurement} 
+    # You must return xy_estimate (x, y), and OTHER (even if it is None) 
+    # in this order for grading purposes.
+
+    if OTHER != None:
+        # measurement update
+        Z = convert_measurement_into_polar_coordinates(measurement, OTHER['prev'])
+        m_heading = Z.value[0][0]
+        m_distance = Z.value[0][1]
+
+        if 'x' not in OTHER.keys():
+            x = matrix([[m_heading], [m_distance], [0.], [0.]]) # initial state (location and velocity)
+            P =  matrix([[0,0,0,0],[0,0,0,0],[0,0,1000,0],[0,0,0,1000]])# initial uncertainty: 0 for positions x and y, 1000 for the two velocities
+        else :
+            x = OTHER['x']
+            P = OTHER['P'] 
+    
+        x, P = kalman_prediction(x, P, Z)
+        
+        heading = x.value[0][0]
+        distance = x.value[1][0]
+    
+        new_x = distance * cos(heading)
+        new_y = distance * sin(heading)
+        xy_estimate = [measurement[0] + new_x, measurement[1] + new_y]
+        OTHER = {'x': x, 'P': P, 'prev': measurement} 
+    else:
+        xy_estimate = measurement
+        OTHER = {'prev': measurement}
+    return xy_estimate, OTHER
 
 # A helper function you may find useful.
 def distance_between(point1, point2):
@@ -154,7 +163,6 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
     measured_broken_robot.penup()
     #End of Visualization
     while not localized and ctr <= 10:
-        time.sleep(.5)
         ctr += 1
         measurement = target_bot.sense()
         position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
@@ -177,7 +185,6 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
         prediction.goto(position_guess[0]*size_multiplier, position_guess[1]*size_multiplier-200)
         prediction.stamp()
         #End of Visualization
-    time.sleep(30)
     return localized
 
 # This is a demo for what a strategy could look like. This one isn't very good.
@@ -192,7 +199,7 @@ def naive_next_pos(measurement, OTHER = None):
 
 # This is how we create a target bot. Check the robot.py file to understand
 # How the robot class behaves.
-test_target = robot(2.1, 4.3, 0.5, 2*pi / 40.0, 1.5)
+test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
 test_target.set_noise(0.0, 0.0, 0.0)
 
 dt = .1
@@ -204,8 +211,5 @@ R =  matrix([[0.00001, 0],[0, 0.00001]])# measurement uncertainty: use 2x2 matri
 I =  matrix([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])# 4d identity matrix
 
 u = matrix([[0.], [0.], [0.], [0.]]) # external motion
-
-# x = (F * x) + u
-# P = F * P * F.transpose()
 
 demo_grading(estimate_next_pos, test_target)
